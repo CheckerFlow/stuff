69a4aebfefadbeca4abe455635e0c4678a6ce4f5
Checker Flow <checkerflow@gmail.com>
2019-11-21 20:15:45 +0000

    Added host config to ApplicationController such that redirects work properly.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def default_url_options
+    if Rails.env.development?
+      {:host => "3000-de969580-f763-4a5e-b66b-1c94a490ea54.ws-eu01.gitpod.io"}
+    else  
+      {}
+    end
+  end    
 end
