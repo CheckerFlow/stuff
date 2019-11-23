06fe17cfa52cfc50db1d218c1ddcd086fb003fb9
Checker Flow <checkerflow@gmail.com>
2019-11-23 16:27:37 +0000

    Created scaffold for storages.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_23_140159) do
+ActiveRecord::Schema.define(version: 2019_11_23_160310) do
 
   create_table "items", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 2019_11_23_140159) do
   end
 
   create_table "rooms", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "storages", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
