faac689551300820370fbbe21e0e08a4199e7663
Checker Flow <checkerflow@gmail.com>
2019-11-26 19:41:43 +0000

    Fixed issue in Webpacker's environment.rb - proper reference to jquery.js.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   def default_url_options
     if Rails.env.development?
-      {:host => "3000-a876d99d-a7e3-4923-a18b-ea229043f460.ws-eu01.gitpod.io"}
+      {:host => "3000-a876d99d-a7e3-4923-a18b-ea229043f460.ws-eu01.gitpod.io/"}
     else  
       {}
     end
