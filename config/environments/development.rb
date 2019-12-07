df1d0ccb4e7a0e2ae2281f2b329b2a6a6442fe20
Checker Flow <checkerflow@gmail.com>
2019-12-07 09:02:40 +0100

    Installed devise. Conducted initial configuration.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,4 +63,16 @@ Rails.application.configure do
   config.web_console.whiny_requests = false
 
   config.action_controller.forgery_protection_origin_check = false
+
+
+  # 1. Ensure you have defined default url options in your environments files. Here
+  #   is an example of default_url_options appropriate for a development environment
+  #   in config/environments/development.rb:
+
+  #     config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  #   In production, :host should be set to the actual host of your application.
+  #
+  
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
