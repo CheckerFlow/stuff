7b87210b2297e4b5622a954eb57bbc34f9ecedfa
Checker Flow <checkerflow@gmail.com>
2019-12-08 10:03:58 +0100

    Created Pages controller with home page for external landing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
     
+  get 'pages/home'
   devise_for :users
   resources :rooms do
     resources :storages, shallow: true
@@ -12,5 +13,5 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root :to => "rooms#index"
+  root :to => "pages#home"
 end
