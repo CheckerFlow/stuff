b6ba0bedd8f7cab4f17c4d776ce9dbbc6764be84
Checker Flow <checkerflow@gmail.com>
2020-01-19 09:38:24 +0100

    Improved implementation of is_shared method.

--- app/helpers/shareable_helper.rb
+++ app/helpers/shareable_helper.rb
@@ -1,18 +1,38 @@
 module ShareableHelper
-    def is_shared(resource)        
 
-        if resource.sharing_group_members.size == 0
-            return false
-        else
-            resource.sharing_group_members.each do 
-                |sharing_group_member|
+    def is_shared(resource)
 
-                if sharing_group_member.user_id == current_user.id
-                    return true
-                end
+        # A resource is shared with the current user if there is a sharing group member of the resource corresponding to the current user's email
+        resource.sharing_group_members.each do 
+            |sharing_group_member|
+            if sharing_group_member.email == current_user.email
+                return true
             end
         end
-        
+
+        # An item is shared if the storage in which it is contained is shared
+        if resource.class == Item
+            item_is_shared = false
+
+            resource.lists.each do 
+                |list|
+                item_is_shared = item_is_shared || is_shared(list)
+            end
+
+            item_is_shared = item_is_shared || is_shared(resource.storage) 
+
+            return item_is_shared
+        end
+
+        if resource.class == Storage
+            return is_shared(resource.room)
+        end
+
+        if resource.class == Room
+            return is_shared(resource.building)
+        end
+
         return false
-    end         
+    end
+  
 end
